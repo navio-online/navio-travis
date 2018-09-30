b9e99351d85599a6af38f05ac087d65f7a3e4856
Peter Salnikov <peter@salnikov.cc>
2018-09-30 14:25:13 +0300

    Version updated to 0.1.3

--- navio/meta_travis.py
+++ navio/meta_travis.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __license__ = "Apache 2.0 license"
 __website__ = "https://oss.navio.online/navio-travis/"
 __download_url__ = ('https://github.com/navio-online/navio-travis/archive'
