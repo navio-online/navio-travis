66135634eb48851cd4d02a0c0c10c9156f2e795d
Peter Salnikov <peter@salnikov.cc>
2020-02-16 17:31:47 +0300

    fix test

--- navio/travis/tests/test_travis.py
+++ navio/travis/tests/test_travis.py
@@ -78,10 +78,10 @@ class Test:
         os.environ['TRAVIS'] = 'true'
         os.environ.pop('TRAVIS_COMMIT', None)
 
-        assert '00000000' == Travis().commit_hash()
+        assert '00000000' == Travis().short_commit_hash()
 
         os.environ['TRAVIS_COMMIT'] = '1f510ab451bb4'
-        assert '1f510ab4' == Travis().commit_hash()
+        assert '1f510ab4' == Travis().short_commit_hash()
 
         os.environ['TRAVIS_COMMIT'] = '04124124bcb131'
-        assert '04124124' == Travis().commit_hash()
+        assert '04124124' == Travis().short_commit_hash()
